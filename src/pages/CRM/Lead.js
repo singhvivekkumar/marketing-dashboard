6dff42b238a688ee1d976801c834d60d58250bee
Vivek Kumar Singh <64981263+singhvivekkumar@users.noreply.github.com>
2025-11-23 23:21:59 +0530

    Refactor the folder structure

--- src/pages/CRM/Lead.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Lead() {
-	return (
-		<div>Lead</div>
-	)
-}
-
-export default Lead
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+// import styled from "@emotion/styled"
+import { Button, Container, Grid, Stack } from "@mui/material";
+
+// const CustomSlider = styled(Slider)(() => ({
+// 	width: "5rem"
+// }))
+
+function Home() {
+	return (
+		<Grid container>
+			<>
+				<Button type="submit">1</Button>
+				<Button type="submit">2</Button>
+				<Button type="submit">3</Button>
+			</>
+		</Grid>
+	);
+}
+
+export default Home;
