d43a7c77c71b3a3fa70eaed44a8acddfe656bf35
Vivek Kumar Singh <64981263+singhvivekkumar@users.noreply.github.com>
2025-11-28 00:26:36 +0530

    Tab Navigation is added

--- src/pages/CrmPage.js
+++ src/pages/CrmPage.js
@@ -1,31 +1,38 @@
-import { Container, Typography } from '@mui/material';
-import LostDomesticLead from '../components/LostDomesticLead';
-import ReceivedDomesticOrder from '../components/ReceiveDomesticOrder';
-import LostDomesticTable from '../components/LostDomesticTable';
-import BudgetaryQuotationForm from '../components/BudgetaryQuotationForm';
-import LeadSubmittedForm from '../components/LeadSubmittedForm';
-import DomesticLeadForm from '../components/DomesticLeadForm';
-import ExportLeadForm from '../components/ExportLeadForm';
-import CRMLeadForm from '../components/CRMLeadForm';
-
+import { Container, Typography } from "@mui/material";
+import LostDomesticLead from "../components/LostDomesticLead";
+import ReceivedDomesticOrder from "../components/ReceiveDomesticOrder";
+import LostDomesticTable from "../components/LostDomesticTable";
+import BudgetaryQuotationForm from "../components/BudgetaryQuotationForm";
+import LeadSubmittedForm from "../components/LeadSubmittedForm";
+import DomesticLeadForm from "../components/DomesticLeadForm";
+import ExportLeadForm from "../components/ExportLeadForm";
+import CRMLeadForm from "../components/CRMLeadForm";
+import NavigationTab from "../components/NavigationTab";
 
 const CrmPage = () => {
 	return (
 		<div>
-			<Typography>BEL Marketing Team</Typography>
-			<Container>
-			{/* <LostDomesticLead/> */}
-			{/* <ReceivedDomesticOrder/> */}
-			{/* <BudgetaryQuotationForm/> */}
-			{/* <LeadSubmittedForm/> */}
-			{/* <DomesticLeadForm/> */}
-			{/* <ExportLeadForm/> */}
-			<CRMLeadForm/>
-			{/*===========To present the data============== */}
-			{/* <LostDomesticTable/> */}
-			</Container>
+			<Typography
+				variant="h4"
+				component="h1"
+				gutterBottom
+				sx={{ fontWeight: 700, color: "#1976d2" }}>
+				BEL Marketing Team - Software SBU
+			</Typography>
+			<>
+				{/* <LostDomesticLead/> */}
+				{/* <ReceivedDomesticOrder/> */}
+				{/* <BudgetaryQuotationForm/> */}
+				{/* <LeadSubmittedForm/> */}
+				{/* <DomesticLeadForm/> */}
+				{/* <ExportLeadForm/> */}
+				{/* <CRMLeadForm/> */}
+				<NavigationTab />
+				{/*===========To present the data============== */}
+				{/* <LostDomesticTable/> */}
+			</>
 		</div>
-	)
-}
+	);
+};
 
 export default CrmPage;
