5a53a224dd277d56a53d415dcf5e3f0b117e762a
Vivek Kumar Singh <64981263+singhvivekkumar@users.noreply.github.com>
2025-11-30 07:30:28 +0530

    I have changed the root dir of api

---
+++ api/app.js
@@ -0,0 +1,21 @@
+// app.js
+const express = require("express");
+const cors = require("cors");
+// const leadRoutes = require("./routes/domesticLeadRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "2mb" }));
+
+// health
+app.get("/health", (req, res) => res.json({ ok: true, data: {data: "You don't have any data..."}, message: 'Health is good' }));
+
+// routes
+// app.use("/api/leads", leadRoutes);
+
+// default 404
+app.use((req, res) => res.status(404).json({ error: "Not found" }));
+
+app.listen(5000, () => console.log(`Server running on ${5000}`));
+
+// module.exports = app;
--- api/index.js
+++ api/index.js
@@ -1,14 +1,16 @@
 // server.js
 require("dotenv").config();
 const app = require("./app");
-const pool = require("./db");
+// const pool = require("./db");
 
 const port = process.env.PORT || 5000;
 
 (async () => {
   try {
+    // first we run database server then we start our backend server
     // test db connection
-    await pool.query("SELECT 1");
+    // await pool.query("SELECT 1");
+    // after database connection we start our backend server
     app.listen(port, () => console.log(`Server running on port ${port}`));
   } catch (err) {
     console.error("Failed to connect to DB", err);
